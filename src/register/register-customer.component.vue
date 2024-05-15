49431c5460a6c4fc99e403d7856bf501fb6f831b
Jorge Juan de Dios <106224225+JorgeJDA@users.noreply.github.com>
2024-05-15 16:26:16 -0500

    Register-Customer
    avance register customer

--- src/register/register-customer.component.vue
+++ src/register/register-customer.component.vue
@@ -1,35 +1,80 @@
 
 <script>
 import router from "../router/index.js";
+import { AccountApiService } from "../services/account-api.service.js";
 import {CustomerApiService} from "../services/customer-api.service.js";
 export default {
   name:"register-customer",
   data() {
     return {
+      //customer
       dni: "",
-      nombre: "",
-      apellido: "",
-      contraseña: "",
-      celular: "",
-      correo: "",
-      customerApiService: new CustomerApiService()
+      first_name: "",
+      last_name: "",
+      password: "",
+      phone: "",
+      email: "",
+
+      //cuenta de credito
+      max_credit: "",
+      current_credit: "",
+      credit_type_of_rate: "",
+      credit_rate: "",
+      //tasa moratoria pago unico
+      penalty_rate: "",
+      //tasa moratorio en cuotas
+      installment_penalty_rate: "",
+      //tasa compensatorio pago unico
+      compensatory_rate: "",
+      //tasa compensatorio en cuotas
+      installment_compensatory_rate: "",
+
+
+      customerApiService: new CustomerApiService(),
+      creditAccount: new AccountApiService()
     };
   },
 
   methods: {
     async create() {
-      const body = {
+      const customer = {
         dni: this.dni,
-        nombre: this.nombre,
-        apellido: this.apellido,
-        contraseña: this.contraseña,
-        celular: this.celular,
-        correo: this.correo,
+        first_name: this.first_name,
+        last_name: this.last_name,
+        password: this.password,
+        phone: this.phone,
+        email: this.email,
+        //cuenta de credito
+        //tipoTasa: "Efectiva",
+        //diaPago: "10",
+        //tasaInteres: "5%",
       };
-      const response = await this.customerApiService.create(body);
+      const response = await this.customerApiService.create(customer);
+
+      const account = { 
+        customer_id:"1",
+        max_credit: "200",
+        current_credit: "200",
+        credit_type_of_rate: "TEM",
+        credit_rate: "5%",
+        penalty_rate: "2%",
+        installment_penalty_rate: "2%",
+        compensatory_rate: "2%",
+        installment_compensatory_rate: "2%",
+      };
+      const response1 = await this.AccountApiService.create(account);
       if (response.status === 201) {
-        alert("Administration Correctamente Registrado.");
-        router.push("admin"); // Corrección aquí
+        alert("Cliente Correctamente Registrado.");
+        router.push({ path: "register-customer" });
+
+        // Limpiar campos después del registro exitoso
+        this.dni = "";
+        this.first_name = "";
+        this.last_name = "";
+        this.password = "";
+        this.phone = "";
+        this.email = "";
+        //fin de limpiar
       } else {
         alert("Error creating");
       }
@@ -48,29 +93,32 @@ export default {
     </div>
     <div class="row">
       <label>Nombre</label>
-      <pv-input-text id="nombre" v-model="nombre"></pv-input-text>
+      <pv-input-text id="first_name" v-model="first_name"></pv-input-text>
     </div>
     <div class="row">
       <label>Apellido</label>
-      <pv-input-text id="apellido" v-model="apellido"></pv-input-text>
+      <pv-input-text id="last_name" v-model="last_name"></pv-input-text>
     </div>
     <div class="row">
       <label>Contraseña</label>
-      <pv-input-text id="contraseña" v-model="contraseña"></pv-input-text>
+      <pv-input-text id="password" v-model="password"></pv-input-text>
     </div>
     <div class="row">
       <label>Celular</label>
-      <pv-input-text id="celular" v-model="celular"></pv-input-text>
+      <pv-input-text id="phone" v-model="phone"></pv-input-text>
     </div>
     <div class="row">
       <label>Correo</label>
-      <pv-input-text id="correo" v-model="correo"></pv-input-text>
+      <pv-input-text id="email" v-model="email"></pv-input-text>
     </div>
     <pv-button @click="create()">save</pv-button>
   </div>
 
 
 </template>
+
+
+
 
 <style scoped>
 
