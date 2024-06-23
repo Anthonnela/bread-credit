efab5f3708881dcd9bba5cbf5b77a021a01e076b
Christian <Christhony2002@gmail.com>
2024-06-23 16:52:53 -0500

    Create payment-api-service.js

---
+++ src/services/payment-api-service.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+
+const http = axios.create({
+    baseURL: 'http://localhost:8080',
+});
+
+export class PaymentApiService {
+    async getAll() {
+        return await http.get('payment');
+    }
+
+    async getById(id) {
+        return await http.get('/payment/' + id);
+    }
+
+    async create(body) {
+        return await http.post('payment', body);
+    }
+
+    async update(body, id) {
+        return await http.put('/payment/' + id, body);
+    }
+
+    async delete(id) {
+        return await http.delete('payment/' + id);
+    }
+}
