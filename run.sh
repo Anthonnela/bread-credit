58503c0da8a83cdc6f617c258d97a9366b554a0f
Fernando Schilder <u20201b814@upc.edu.pe>
2024-06-24 02:44:33 -0500

    fiix

---
+++ run.sh
@@ -0,0 +1,2 @@
+sudo docker build -t dockerize-vuejs-app-1 .
+sudo docker run -it -p 80:80 --rm --name dockerize-vuejs-app-1 dockerize-vuejs-app-1
--- src/services/product.service.js
+++ src/services/product.service.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const http = axios.create({
-    baseURL: 'http://localhost:8080',
+    baseURL: 'http://20.150.213.225:8080',
 });
 
 export class ProductService {
